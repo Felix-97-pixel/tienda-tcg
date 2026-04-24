888b70b0da14cfe2bf32531c260e519541c8aed6
Felix Cabello Santelices <felix.cabello97@gmail.com>
2026-04-24 17:10:59 -0400

    cambios para el sitio para cambiar la descropcion

--- tienda-front/src/app/(site)/page.tsx
+++ tienda-front/src/app/(site)/page.tsx
@@ -2,9 +2,8 @@ import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextCommerce | Nextjs E-commerce template",
-  description: "This is Home for NextCommerce Template",
-  // other metadata
+  title: "TapTrade",
+  description: "Tienda especializada en TCG",
 };
 
 export default function HomePage() {
