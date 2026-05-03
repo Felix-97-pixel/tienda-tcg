51ad66b703f661ab2be12d5340e7d944cde9b350
Felix Cabello Santelices <felix.cabello97@gmail.com>
2026-05-03 03:06:42 -0400

    cambios para que cuando el token expire se deslogee

--- tienda-front/src/redux/provider.tsx
+++ tienda-front/src/redux/provider.tsx
@@ -2,8 +2,26 @@
 
 import { store } from "./store";
 import { Provider } from "react-redux";
-import React from "react";
+import { useState, useEffect } from "react";
+import { logout } from "./features/auth-slice";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const originalFetch = window.fetch;
+      window.fetch = async (...args) => {
+        const response = await originalFetch(...args);
+        // Deslogear si expira el token y no es la ruta de login
+        if (response.status === 401) {
+          const isLogin = typeof args[0] === 'string' && args[0].includes('/auth/login');
+          if (!isLogin) {
+            store.dispatch(logout());
+          }
+        }
+        return response;
+      };
+    }
+  }, []);
+
   return <Provider store={store}>{children}</Provider>;
 }
