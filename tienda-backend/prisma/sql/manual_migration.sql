2e4158b5f4c73b50abfa0c2538a4810b9a8c6c02
Felix Cabello Santelices <felix.cabello97@gmail.com>
2026-06-12 02:27:19 -0400

    cambios , eliminacion de codigo basura y ademas de creacion de carpeta de triggers para que quede claro

--- tienda-backend/prisma/sql/manual_migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "CardDetail" ALTER COLUMN "game" TYPE TEXT USING "game"::text;
-DROP TYPE "GameType";
-
-ALTER TABLE "WishlistItem" DROP COLUMN "id";
-ALTER TABLE "WishlistItem" ADD PRIMARY KEY ("userId", "productId");
